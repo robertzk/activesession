b4f7262e5830dd267a2ea67c62b33cb32767b55a
robertzk <technoguyrob@gmail.com>
2017-04-06 22:21:06 -0500

    Document undocumented arguments.

--- R/backend.R
+++ R/backend.R
@@ -6,6 +6,7 @@
 #' @param name character. Name of the activesession backend. Currently,
 #'   supported values are \code{c("r", "file", "s3mpi", "cacher")}, but the
 #'   latter two require additional packages.
+#' @param ... additional options to pass to the specific backend.
 #' @export
 #' @return An \code{activesession_backend} R6 object.
 activesession_backend <- function(name = c("r", "file", "s3mpi", "cacher"), ...) {
--- R/utils.R
+++ R/utils.R
@@ -13,6 +13,10 @@ is.directory <- function(path) {
 #' Checks if a string is a simple string.
 #'
 #' A simple string is (a) character, (b) length 1, and (c) not blank.
+#'
+#' @param string character. The object to test.
+#' @return \code{TRUE} or \code{FALSE} according as the object is a
+#'   non-NA non-blank string of length 1.
 #' @export
 is.simple_string <- function(string) {
   is.character(string) && length(string) == 1 && nzchar(string) && !is.na(string)
