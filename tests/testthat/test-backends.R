cade6c3cf885a9cf3321942e94c572621f9b90ef
robertzk <technoguyrob@gmail.com>
2017-04-06 20:52:37 -0500

    Test that it can read and write to an in-memory backend.

--- tests/testthat/test-backends.R
+++ tests/testthat/test-backends.R
@@ -1,4 +1,5 @@
 context("activesession backends")
+library(testthatsomemore)
 
 describe("activesession_backend", {
   describe("failure conditions", {         
@@ -7,6 +8,22 @@ describe("activesession_backend", {
       expect_error(activesession_backend(list()))
       expect_error(activesession_backend(identity))
       expect_error(activesession_backend(new.env()))
+    })
+  })
+
+  describe("r backend", {
+    test_that("it can initialize an r backend", {
+      assert(activesession_backend("r"))
+    })
+
+    test_that("it can store an object", {
+      assert(activesession_backend("r")$write(list(a = 1, b = 2), "foo"))
+    })
+
+    test_that("it can read an object", {
+      session <- activesession_backend("r")       
+      session$write(list(a = 1, b = 2), "foo")
+      expect_equal(session$read("foo"), list(a = 1, b = 2))
     })
   })
 })
